e03e870b4a7b128c66eab47188fe055b993fe5e9
Phillip Hilliard <phillip.hilliard@jhuapl.edu>
2017-08-02 13:28:30 -0400

    Created a simple functional "login" system. It stores names in a local array and lets the user log into a dashboard using a name. ToDo: Turn that system into a complete login feature. Also stylize the HTML.

--- ComicTracker/app.py
+++ ComicTracker/app.py
@@ -1,47 +1,61 @@
 from flask import Flask, render_template, request, json, redirect, url_for, Response
 
-
+names = []
 app=Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def homepage():
-    return(render_template('index.html'))
+    print("names="+str(names))
+    return(render_template("index.html"))
 
 
 
 # this is how you can send or receive stuff
-@app.route('/api', methods=['POST', 'GET'])
+@app.route("/api", methods=["POST", "GET"])
 def api_test():
-    if request.method=='POST':
-        if 'greeting' in request.form.keys():
+    if request.method=="POST":
+        if "greeting" in request.form.keys():
             return(json.jsonify(message="valid!"))
-        response = json.jsonify({'message':'invalid'})
+        response = json.jsonify({"message":"invalid"})
         response.status_code = 400
         return(response)
-    elif request.method=='GET':
+    elif request.method=="GET":
         return(json.jsonify(message="gotten"))
 
 
+@app.route("/register")
+def register(failed=False):
+    return(render_template("register.html", failed=failed))
 
-@app.route('/login')
+@app.route("/login")
 def login(failed=False):
-    return(render_template('login', failed=failed))
+    return(render_template("login.html", failed=failed))
 
-@app.route('/loginattempt', methods=['POST'])
+@app.route("/registerattempt", methods=["POST"])
+def register_attempt():
+    if "name" in request.form.keys():
+        if not(request.form['name'] in names):
+            names.append(request.form['name'])
+            return(redirect(url_for("login", failed=False)))
+
+    return(redirect(url_for("register", failed=True)))
+
+
+@app.route("/loginattempt", methods=["POST"])
 def login_attempt():
-    if 'name' in request.form.keys():
-        return(redirect(url_for('dashboard', name=request.form['name'])))
-    else:
-        return(redirect(url_for('login')))
+    if "name" in request.form.keys():
+        print(names)
+        if request.form["name"] in names:
+            return(redirect(url_for("dashboard", name=request.form["name"])))
 
-@app.route('/dashboard')
-def dashboard(name=''):
-    if name=='':
-        return(redirect(url_for('login', failed=True)))
-    else:
-        return('<html><h1>Hello,'+name+'.</h1></html>')
+    return(redirect(url_for("login", failed=True)))
 
-if(__name__=='__main__'):
+@app.route("/dashboard/<name>")
+def dashboard(name=""):
+    if name!="":
+        return("<html><h1>Hello, "+name+".</h1></html>")
+    return(redirect(url_for("login", failed=True)))
+if(__name__=="__main__"):
     app.run(debug=True)
 
 
