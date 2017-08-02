75284f47646d1292b3ec32d05822c762127c0a38
Phillip Hilliard <phillip.hilliard@jhuapl.edu>
2017-08-02 14:58:34 -0400

    added logout and session data storing

--- ComicTracker/app.py
+++ ComicTracker/app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, json, redirect, url_for, Response
+from flask import Flask, render_template, request, session, json, redirect, url_for, Response
 
 names = []
 app=Flask(__name__)
 
 @app.route("/")
 def homepage():
-    print("names="+str(names))
     return(render_template("index.html"))
-
-
 
 # this is how you can send or receive stuff
 @app.route("/api", methods=["POST", "GET"])
@@ -22,39 +19,50 @@ def api_test():
     elif request.method=="GET":
         return(json.jsonify(message="gotten"))
 
-
 @app.route("/register")
 def register(failed=False):
     return(render_template("register.html", failed=failed))
 
 @app.route("/login")
 def login(failed=False):
+    if "name" in session:
+        return (redirect(url_for("dashboard", name=session["name"])))
     return(render_template("login.html", failed=failed))
 
 @app.route("/registerattempt", methods=["POST"])
 def register_attempt():
     if "name" in request.form.keys():
-        if not(request.form['name'] in names):
-            names.append(request.form['name'])
+        if not(request.form["name"] in names):
+            names.append(request.form["name"])
             return(redirect(url_for("login", failed=False)))
-
     return(redirect(url_for("register", failed=True)))
-
 
 @app.route("/loginattempt", methods=["POST"])
 def login_attempt():
     if "name" in request.form.keys():
         print(names)
         if request.form["name"] in names:
+            session["name"] = request.form["name"]
             return(redirect(url_for("dashboard", name=request.form["name"])))
-
     return(redirect(url_for("login", failed=True)))
+
+@app.route("/logout")
+def logout():
+    session.pop("name", None)
+    return(redirect(url_for("login")))
+
+@app.route("/dashboard")
+def dashboard_no_name():
+    if "name" in session:
+        return(redirect(url_for("dashboard", name=session["name"])))
+    return(redirect((url_for("login", failed=True))))
 
 @app.route("/dashboard/<name>")
 def dashboard(name=""):
     if name!="":
         return("<html><h1>Hello, "+name+".</h1></html>")
     return(redirect(url_for("login", failed=True)))
+
 if(__name__=="__main__"):
     app.run(debug=True)
 
